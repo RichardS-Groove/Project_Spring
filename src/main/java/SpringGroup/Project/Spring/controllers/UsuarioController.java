c8acc533d6e8bab645b47505b76cb2936b023f38
Richard Campos <richardsjcb@gmail.com>
2022-02-05 15:58:20 -0300

    IM-S# First Commit

---
+++ src/main/java/SpringGroup/Project/Spring/controllers/UsuarioController.java
@@ -0,0 +1,15 @@
+package SpringGroup.Project.Spring.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+
+    @RequestMapping(value = "prueba")
+    public List<String> prueba(){
+        return List.of("Manzana", "Kiwi", "Banana");
+    }
+}
---
+++ src/test/java/SpringGroup/Project/Spring/ProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringGroup.Project.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
